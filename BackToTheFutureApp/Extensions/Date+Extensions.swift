dca2b858f5afa52d6d6a6f1616975d5093c7f490
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-05-23 19:36:27 -0300

    implementado método formatedDate

--- BackToTheFutureApp/Extensions/Date+Extensions.swift
+++ BackToTheFutureApp/Extensions/Date+Extensions.swift
@@ -3,14 +3,20 @@ import Foundation
 extension Date {
     
     func dateAfter(years: Int) -> Date? {
-     
+        
         // TODO
         return nil
     }
     
     func formattedDate() -> String {
+        let setDateFormatter = DateFormatter()
+        setDateFormatter.dateFormat = "dd-MM-yyyy"
+        let newDate = setDateFormatter.string(from: <#T##Date#>)
+        return newDate
         
-        // TODO
-        return ""
     }
 }
+
+    
+
+
